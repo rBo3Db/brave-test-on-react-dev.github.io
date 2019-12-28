f1376bab2ca96b5003ff9ef585d0ae6d71fea9b1
rBo3Db <rBo3Db@mail.ru>
2019-12-28 18:43:25 +0400

    added hooks

--- src/components/PayPage.jsx
+++ src/components/PayPage.jsx
@@ -1,48 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import InputMask from 'react-input-mask';
 import { Loader } from './Loader';
 import { Popup } from './Popup';
 import { Button } from './Button';
 import styled from 'styled-components';
 
-export class PayPage extends React.Component {
-    constructor() {
-        super();
-        this.changeLoaderState = this.changeLoaderState.bind(this);
-        this.changePopupState = this.changePopupState.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.resetPopupState = this.resetPopupState.bind(this);
+export function PayPage(props) {
+    React.useState();
+
+    const [operator, setOperator] = useState('');
+    const [isLoaderShow, setLoaderShow] = useState(false);
+    const [apiStatus, setApiStatus] = useState(null);
+
+    useEffect(() => {
+        const { name } = props.match.params;
+        setOperator(name);
+    },[])
+
+    const changeLoaderState = (newIsShownState) => {
+        setLoaderShow(newIsShownState);
     }
-    state = {
-        operator: '',
-        isLoaderShow: false,
-        apiStatus: null,
+    const changePopupState = (newApiStatus) => {
+        setApiStatus(newApiStatus);
     }
-    componentDidMount () {
-        const { name } = this.props.match.params;
-        this.setState({operator: name})
-    }
-    changeLoaderState(newIsShownState) {
-        this.setState({
-            ...this.state,
-            isLoaderShow: newIsShownState
-        });
-    }
-    changePopupState(newApiStatus) {
-        this.setState({
-            ...this.state,
-            apiStatus: newApiStatus
-        });
-    }
-    resetPopupState() {
-        this.changePopupState(null);
+    const resetPopupState = () => {
+        changePopupState(null);
     }
     
 
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault();
         const responseTime = Math.round(Math.random() * 5) * 1000;
-        this.changeLoaderState(true);
+        changeLoaderState(true);
         new Promise((resolve, reject) => {
             setTimeout(() => {
                 Math.round(Math.random()) ? resolve() : reject(new Error())
@@ -50,58 +39,154 @@ export class PayPage extends React.Component {
         })
         .then(
             result => {
-                this.changeLoaderState(false);
-                this.changePopupState('success');
+                changeLoaderState(false);
+                changePopupState('success');
             },
             error => {
-                this.changeLoaderState(false);
-                this.changePopupState('error');
+                changeLoaderState(false);
+                changePopupState('error');
             }
         );
     }
-    render() {
-        return(
-            <PayForm>
-                <Loader isShown = {this.state.isLoaderShow}/>
-                <Popup
-                    apiStatus = {this.state.apiStatus}
-                    resetPopupState={this.resetPopupState}    
+
+    return(
+        <PayForm>
+            <Loader isShown = {isLoaderShow}/>
+            <Popup
+                apiStatus = {apiStatus}
+                resetPopupState={resetPopupState}    
+            />
+            <Header>
+                Pay for {operator} 
+            </Header>
+            <form onSubmit={handleSubmit}>
+                <p>Please enter phone number</p>
+                <InputMask
+                    id="phoneNumber"
+                    type="tel"
+                    placeholder="Phone number" 
+                    name="phone"
+                    pattern="^\+7 \(\d{3}\) \d{3} \d{2} \d{2}$"
+                    title="+7 (999) 999 99 99"
+                    mask="+7 (999) 999 99 99"
+                    size="18"
+                    required
                 />
-                <Header>
-                    Pay for {this.state.operator} 
-                </Header>
-                <form onSubmit={this.handleSubmit}>
-                    <p>Please enter phone number</p>
-                    <InputMask
-                        id="phoneNumber"
-                        type="tel"
-                        placeholder="Phone number" 
-                        name="phone"
-                        pattern="^\+7 \(\d{3}\) \d{3} \d{2} \d{2}$"
-                        title="+7 (999) 999 99 99"
-                        mask="+7 (999) 999 99 99"
-                        size="18"
-                        required
-                    />
-                    <p>Please enter payment amount</p>
-                    <InputMask
-                        id="paymentAmount"
-                        type="num"
-                        placeholder="Payment amount" 
-                        name="money"
-                        pattern="^\d{1,3}$|^1000$"
-                        title="from 1 to 1000"
-                        mask="9999"
-                        maskChar=""
-                        size="18"
-                        required
-                    />
-                    <Button type="submit">Continue</Button>
-                </form>
-            </PayForm>
-        )
-    }
-}
+                <p>Please enter payment amount</p>
+                <InputMask
+                    id="paymentAmount"
+                    type="num"
+                    placeholder="Payment amount" 
+                    name="money"
+                    pattern="^\d{1,3}$|^1000$"
+                    title="from 1 to 1000"
+                    mask="9999"
+                    maskChar=""
+                    size="18"
+                    required
+                />
+                <Button type="submit">Continue</Button>
+            </form>
+        </PayForm>
+    )
+};
+// export class PayPage extends React.Component {
+//     constructor() {
+//         super();
+//         this.changeLoaderState = this.changeLoaderState.bind(this);
+//         this.changePopupState = this.changePopupState.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//         this.resetPopupState = this.resetPopupState.bind(this);
+//     }
+//     state = {
+//         operator: '',
+//         isLoaderShow: false,
+//         apiStatus: null,
+//     }
+//     componentDidMount () {
+//         const { name } = this.props.match.params;
+//         this.setState({operator: name})
+//     }
+//     changeLoaderState(newIsShownState) {
+//         this.setState({
+//             ...this.state,
+//             isLoaderShow: newIsShownState
+//         });
+//     }
+//     changePopupState(newApiStatus) {
+//         this.setState({
+//             ...this.state,
+//             apiStatus: newApiStatus
+//         });
+//     }
+//     resetPopupState() {
+//         this.changePopupState(null);
+//     }
+    
+
+//     handleSubmit(event) {
+//         event.preventDefault();
+//         const responseTime = Math.round(Math.random() * 5) * 1000;
+//         this.changeLoaderState(true);
+//         new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 Math.round(Math.random()) ? resolve() : reject(new Error())
+//             }, responseTime)
+//         })
+//         .then(
+//             result => {
+//                 this.changeLoaderState(false);
+//                 this.changePopupState('success');
+//             },
+//             error => {
+//                 this.changeLoaderState(false);
+//                 this.changePopupState('error');
+//             }
+//         );
+//     }
+//     render() {
+//         return(
+//             <PayForm>
+//                 <Loader isShown = {this.state.isLoaderShow}/>
+//                 <Popup
+//                     apiStatus = {this.state.apiStatus}
+//                     resetPopupState={this.resetPopupState}    
+//                 />
+//                 <Header>
+//                     Pay for {this.state.operator} 
+//                 </Header>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <p>Please enter phone number</p>
+//                     <InputMask
+//                         id="phoneNumber"
+//                         type="tel"
+//                         placeholder="Phone number" 
+//                         name="phone"
+//                         pattern="^\+7 \(\d{3}\) \d{3} \d{2} \d{2}$"
+//                         title="+7 (999) 999 99 99"
+//                         mask="+7 (999) 999 99 99"
+//                         size="18"
+//                         required
+//                     />
+//                     <p>Please enter payment amount</p>
+//                     <InputMask
+//                         id="paymentAmount"
+//                         type="num"
+//                         placeholder="Payment amount" 
+//                         name="money"
+//                         pattern="^\d{1,3}$|^1000$"
+//                         title="from 1 to 1000"
+//                         mask="9999"
+//                         maskChar=""
+//                         size="18"
+//                         required
+//                     />
+//                     <Button type="submit">Continue</Button>
+//                 </form>
+//             </PayForm>
+//         )
+//     }
+// }
 
 const PayForm = styled.div`
     background-color: white;
